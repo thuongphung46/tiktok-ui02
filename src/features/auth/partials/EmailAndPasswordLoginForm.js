db6cec8f3e8fd2fedf1a1ba7101576fe7e6be6f5
thuongphung46 <ct040249@actvn.edu.vn>
2022-12-07 10:13:55 +0700

    chore: css login

--- src/features/auth/partials/EmailAndPasswordLoginForm.js
+++ src/features/auth/partials/EmailAndPasswordLoginForm.js
@@ -1,6 +1,6 @@
-import { useContext, useState } from 'react'
-import { ModalBodyNameContext } from '../../../components/Layout/components/Header'
-import * as authService from '~/services/authService'
+import { useContext, useState } from 'react';
+import { ModalBodyNameContext } from '../../../components/Layout/components/Header';
+import * as authService from '~/services/authService';
 import classNames from 'classnames/bind';
 import styles from './Login.module.scss';
 import Button from 'components/Button';
@@ -8,46 +8,45 @@ import Button from 'components/Button';
 const cs = classNames.bind(styles);
 
 function EmailAndPasswordLoginForm() {
-  const value = useContext(ModalBodyNameContext)
+  const value = useContext(ModalBodyNameContext);
   // const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [email, setEmail] = useState('')
-
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
 
   const loginUser = () => {
     authService
       .login(email, password)
       .then((data) => {
         if (data.meta && data.meta.token) {
-          localStorage.setItem('user', JSON.stringify(data))
-          alert('login successful!')
-          window.location.reload()
+          localStorage.setItem('user', JSON.stringify(data));
+          alert('login successful!');
+          window.location.reload();
         } else {
-          alert('email or password is invalid! Please try again')
+          alert('email or password is invalid! Please try again');
         }
       })
       .catch((error) => {
-        console.log(error)
-      })
-  }
+        console.log(error);
+      });
+  };
 
-
- 
   return (
     <>
       <div className={cs('overflow-auto')} style={{ flex: '1 1 0%' }}>
-        <div className={cs('wrapper-menu-level2')}/*"m-auto w-4/5"*/>
-          <h3 className={cs('text-center')}/*"text-center text-2xl md:text-3xl lg:text-4xl font-bold my-4 mx-auto"*/>Log in</h3>
+        <div className={cs('wrapper-menu-level2')} /*"m-auto w-4/5"*/>
+          <h3 className={cs('text-center')} /*"text-center text-2xl md:text-3xl lg:text-4xl font-bold my-4 mx-auto"*/>
+            Log in
+          </h3>
 
           <form onSubmit={loginUser}>
-            <div className={cs('text-base')}/*"text-base font-semibold flex justify-between mb-2"*/>
+            <div className={cs('text-base')} /*"text-base font-semibold flex justify-between mb-2"*/>
               <label>Email or username</label>
               <a
                 href="#/"
                 className="font-semibold text-xs hover:underline text-black/60"
                 onClick={(event) => {
-                  event.preventDefault()
-                  value.handleModalBodyName('login-with-phone')
+                  event.preventDefault();
+                  value.handleModalBodyName('login-with-phone');
                 }}
               >
                 Log in with phone
@@ -67,7 +66,7 @@ function EmailAndPasswordLoginForm() {
             <div className={cs('mb-2')}>
               <input
                 className={cs('rounded')}
-                style={{ paddingInlineStart: '12px', paddingInlineEnd: '12px',fontSize:'20px' }}
+                style={{ paddingInlineStart: '12px', paddingInlineEnd: '12px', fontSize: '20px' }}
                 name="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -78,7 +77,7 @@ function EmailAndPasswordLoginForm() {
             <div className={cs('mb-2')}>
               <input
                 className={cs('rounded')}
-                style={{ paddingInlineStart: '12px', paddingInlineEnd: '12px', fontSize:'20px'}}
+                style={{ paddingInlineStart: '12px', paddingInlineEnd: '12px', fontSize: '20px' }}
                 name="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -88,25 +87,27 @@ function EmailAndPasswordLoginForm() {
             </div>
             <a
               href="#/"
-              className ={cs('font-semibold')}
-              onClick = {(event) => {
-                event.preventDefault()
-                value.handleModalBodyName('reset-password-with-email')
+              className={cs('font-semibold')}
+              onClick={(event) => {
+                event.preventDefault();
+                value.handleModalBodyName('reset-password-with-email');
               }}
             >
               Forget password?
             </a>
-            <Button primary
-              className={cs('mt-8')} /*"mt-8 border-none bg-primary text-white text-base leading-5 font-bold font-primary rounded flex items-center justify-center w-full cursor-pointer py-1.5 px-2"*/
+            <Button
+              primary
+              outline
+              className="mt-8 border-none text-base leading-5 font-bold font-primary rounded flex w-full cursor-pointer py-1.5 px-2"
               style={{
                 minWidth: '120px',
                 minHeight: '46px',
+                backgroundColor: 'var(--primary)',
               }}
-              type='submit'
-
+              type="submit"
               onClick={(e) => {
-                e.preventDefault()
-                loginUser()
+                e.preventDefault();
+                loginUser();
               }}
             >
               Log in
@@ -115,7 +116,7 @@ function EmailAndPasswordLoginForm() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default EmailAndPasswordLoginForm
+export default EmailAndPasswordLoginForm;
